54a3bb1ff3fc2c546861ac116bc571ec8f5726d9
Yulia Suslova <suslova.dzhulia2017@yandex.ru>
2022-05-22 15:30:30 +0300

    fix

--- src/EchoServer.java
+++ src/EchoServer.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 
@@ -12,7 +13,7 @@ public class EchoServer {
     private Connection connection;
     private LinkedTransferQueue<SimpleMessage> messages;
     private HashSet<Connection> connections; //CopyOnWriteArraySet ? нельзя использовать так как нельзя использовать метод remove??
-private  ArrayList<Future<SimpleMessage>> results;
+    private ArrayList<Future<SimpleMessage>> results;
 
     public EchoServer(int port) {
         this.port = port;
@@ -38,21 +39,22 @@ private  ArrayList<Future<SimpleMessage>> results;
                 //создается потоки на одного клиента один поток, сколько клиентов столько потоков (нефиксированное кол-во потоков)
                 //пул потоко сендмесадж
                 ExecutorService pool = Executors.newFixedThreadPool(10);
-               // ReceiveMessage receiveMessage1 = new ReceiveMessage(socket);
-               Callable<SimpleMessage> receiveMessage=new ReceiveMessage(socket);
-              ArrayList<Future<SimpleMessage>> results = new ArrayList<>();
+                // ReceiveMessage receiveMessage1 = new ReceiveMessage(socket);
+                Callable<SimpleMessage> receiveMessage = new ReceiveMessage(socket);
+
+                ArrayList<Future<SimpleMessage>> results = new ArrayList<>();
                 for (int i = 0; i < 10; i++) {
                     Future<SimpleMessage> resultContainer = pool.submit(receiveMessage);
                     results.add(resultContainer);
                     System.out.println("Сообщение из контейнера: " + results.get(i));
                 }
-              //  for (Future<SimpleMessage> simpleMessageFuture: results) {
-              //      System.out.println(simpleMessageFuture);
-               // }
-               // connection = new Connection(socket); //у потока должны быть ссылка на подключение
+                //  for (Future<SimpleMessage> simpleMessageFuture: results) {
+                //      System.out.println(simpleMessageFuture);
+                // }
+                // connection = new Connection(socket); //у потока должны быть ссылка на подключение
                 // ссылку на подключение нужно передать в поток получатель в потокобезопасную коллекцию
-               // printMessage(connection.readMessage());
-               // connection.sendMessage(SimpleMessage.getMessage("server", "сообщение получено"));
+                // printMessage(connection.readMessage());
+                // connection.sendMessage(SimpleMessage.getMessage("server", "сообщение получено"));
 
             }
         }
@@ -69,6 +71,7 @@ private  ArrayList<Future<SimpleMessage>> results;
         public ReceiveMessage(Socket socket) throws IOException {
             this.socket = socket;
             this.connection = new Connection(socket);
+
         }
 
         @Override
@@ -79,14 +82,14 @@ private  ArrayList<Future<SimpleMessage>> results;
             connections.add(connection);
             messages.transfer(message);
             printMessage(message);
-            if (socket.getInputStream().read()==-1){
+            if (socket.getInputStream().read() == -1) {
                 connections.remove(connection);
             }
             while (true) {
                 SimpleMessage nextMessage = connection.readMessage();
                 messages.put(nextMessage);
                 messages.transfer(nextMessage);
-                System.out.println("Сообщение отправлено: "+ nextMessage);
+                System.out.println("Сообщение отправлено: " + nextMessage);
                 return nextMessage;
             }
         }
@@ -99,7 +102,7 @@ private  ArrayList<Future<SimpleMessage>> results;
             try {
                 SimpleMessage simpleMessage = messages.take();
                 for (Connection connection : connections) {
-                    if (connection.getClient() != simpleMessage.getSender()) {
+                    if (!Objects.equals(connection.getClient(), simpleMessage.getSender())) {
                         connection.sendMessage(simpleMessage);
                     }
                 }
